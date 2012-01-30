0efac7fb1db74c1cba3e7dfc8e32330e083b7b23
Matthias Mees <mm@yellowled.de>
2012-01-30 01:10:44 +0100

    Replaced entrypaging icons with CSS3-only solution.

--- js/2k11.js
+++ js/2k11.js
@@ -16,9 +16,6 @@ jQuery(document).ready(function($) {
     var $select = $('<select/>');
     $('#primary-nav li').each(function(){var $el=$(this);if($el.find('span').length){$('<option/>',{'selected':'selected','value':'','text':$el.text()}).appendTo($select);}else{$('<option/>',{'value':$el.find('a').attr('href'),'text':$el.text()}).appendTo($select);}});
     if($select.children().size()>0){$select.appendTo('#primary-nav').change(function(){window.location=$(this).find('option:selected').val();});}
-    // Replace entrypaging icons
-    $('.serendipity_entrypaging_left .epicon').text('←');
-    $('.serendipity_entrypaging_right .epicon').text('→');
     // Fit embedded videos
     $('#content>article').fitVids();
     // placeholder polyfill
